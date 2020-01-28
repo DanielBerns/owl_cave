279262ad19514231a495f1869a849d5e33d0b381
Peter Schueler <peter@orangemirrorentertainment.com>
2020-01-28 10:54:33 +0100

    Added First Route And Templates For Game

--- owl_cave/app/routes.py
+++ owl_cave/app/routes.py
@@ -1,6 +1,12 @@
 from flask import render_template
 from app import app
+from app.models import Game, GameNode
 
 @app.route('/')
 def index():
 	return render_template('index.html', title='owl_cave')
+	
+@app.route('/<game_id>')
+def game(game_id):
+	game = Game
+	return render_template('game_html')
